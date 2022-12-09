49623a8cfa0abc157eacebfc9830f3158fb84977
adaptingadapting <elpatoninjasuperninja@gmail.com>
2022-12-09 14:26:58 -0800

    added own getenv

---
+++ getenv.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * _getenv - get and environment var
+ * @str: string
+ * Return: pointer to tring
+ */
+
+char *_getenv(char *str)
+{
+	int i;
+	char *buffer = NULL;
+	char *token[4] = {0};
+	char *rturnString;
+
+	for (i = 0; environ[i]; i++)
+	{
+		buffer = malloc(_strlen(environ[i]) + 1);
+		_strcpy(buffer, environ[i]);
+		token[0] = strtok(buffer, "=\n");
+		if (!(_strcmp(token[0], str)))
+		{
+			token[1] = strtok(NULL, "=\n");
+			rturnString = malloc(_strlen(token[1]) + 1);
+			_strcpy(rturnString, token[1]);
+			free(buffer);
+			return (rturnString);
+		}
+		free(buffer);
+	}
+	return (NULL);
+}
