8a7e1ee82c7d386f3438a95f4e1710b3be89ae5c
adaptingadapting <elpatoninjasuperninja@gmail.com>
2022-12-10 09:07:17 -0800

    removed extra file

--- AUTHORS.c
+++
@@ -1,4 +0,0 @@
-# This is the list of Holberton's Simple Shell's significant contributors.
-
-Agustín Curutchet <5739@holbertonstudents.com>
-IV-an Ariel Rodríguez Pointin <5755@holbertonstudents.com>
