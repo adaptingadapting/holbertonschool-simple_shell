e3840f27ab01887f3c6f9cbd3167f13dfe788b1c
adaptingadapting <elpatoninjasuperninja@gmail.com>
2022-12-13 15:30:03 -0800

    now exit status of exited child returns

--- execute.c
+++ execute.c
@@ -4,23 +4,31 @@
 /**
  * executePathProgram - executes a program
  * @token: an array of pointers to strings
+ * Return: status
  */
 
-void executePathProgram(char **token)
+int executePathProgram(char **token)
 {
 	int status;
 	pid_t pid;
+	int exit_status;
 
 	pid = fork();
 	if (pid == -1)
-		perror("failed to fork");
+		exit(1);
 	else if (pid > 0)
+	{
 		wait(&status);
+		exit_status = WEXITSTATUS(status);
+	}
 	else
 	{
 		if (execve(token[0], token, environ) == -1)
 		{
 			exit(EXIT_FAILURE);
 		}
+
 	}
+	return (exit_status);
 }
+
