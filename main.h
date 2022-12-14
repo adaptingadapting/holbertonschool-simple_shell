48973f27f41bc8111f05fcaff7e392262e30de63
adaptingadapting <elpatoninjasuperninja@gmail.com>
2022-12-13 16:36:47 -0800

    added 4 functions to main .h

--- main.h
+++ main.h
@@ -13,10 +13,12 @@
 
 extern char **environ;
 
+void free3(char *str, char *str2, char *str1);
+void free4(char *s, char *s1, char *s2, char *s3);
 char *programStat(char *line);
 int executePathProgram(char **token);
 void tokenize(char **token, char *delim);
-int xcuteFunc(char *string, char **token);
+int xcuteFunc(char *string, char **token, int *pcount);
 int _strlen(char *str);
 int _strcmp(char *str1, char *str2);
 char *_strcpy(char *dest, char *src);
